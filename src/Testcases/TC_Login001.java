b4783c0321d421000a71e99ece0b766224cae075
shariquerahmetullah <shariquerahmatullah383@gmail.com>
2023-11-23 12:10:31 +0530

    "Pushing Empty Framework"

---
+++ src/Testcases/TC_Login001.java
@@ -0,0 +1,9 @@
+package Testcases;
+
+public class TC_Login001 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+	}
+}
---
+++ src/Testcases/TC_Login002.java
@@ -0,0 +1,9 @@
+package Testcases;
+
+public class TC_Login002 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+	}
+}
